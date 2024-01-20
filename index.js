abbf29b16871ffae1db9157fdf232a90ccca5f52
Umair Ahmed <umairghani156@gmail.com>
2024-01-20 14:25:01 +0500

    serverJsChange

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/profile', (req, res)=>{
 })
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=>{
     console.log("Server is listening on the port " + PORT);
 })
